4864958c3732d21355b0136bc163b43545629cbf
Andreas Jacobsen <andreas1300@gmail.com>
2019-05-23 00:20:31 +0200

    added a turnarny condtional going to null

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ function isAuthed() {
   return true;
 }
 
-function isNew() {
-  return false;
+function showWarning() {
+  return true;
 }
+
 function App() {
   const authed = isAuthed();
-  const firstLogih = isNew();
+  const warning = showWarning();
 
   return (
     <div className="App">
-      <h1>
-        {/*Turnary operator*/}
-        {authed === true ? <h1>Welcome back!</h1> : <h1>Please log inn</h1>}
-      </h1>
+      {/*Turnary operator*/}
+      {authed === true ? <h1>Welcome back!</h1> : <h1>Please log inn</h1>}
+      {warning === true ? (
+        <h1>Warning er sann!</h1>
+      ) : null /* returner null så komponent bare viser noe om conditonen er rett*/}
     </div>
   );
 }
