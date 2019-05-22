7a42a567255cb60e6edfde082d6ad97621287305
Andreas Jacobsen <andreas1300@gmail.com>
2019-05-23 00:12:01 +0200

    changed syntax from JS functions to JSX

--- src/App.js
+++ src/App.js
@@ -13,26 +13,13 @@ function App() {
   const authed = isAuthed();
   const firstLogih = isNew();
 
-
-  if (firstLogih === true) {
-    return (
-      <div className="App">
-        <h1>Welcome to our platform first time user</h1>
-      </div>
-    );  }
-  if (authed === true) {
-    return (
-      <div className="App">
-        <h1>Hello Logged inn user</h1>
-      </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <h1>Login to see your dashboard</h1>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>
+        {authed === true ? <h1>Welcome back!</h1> : <h1>Please log inn</h1>}
+      </h1>
+    </div>
+  );
 }
 
 export default App;
