b3ace5825f1d8ee4b6818195aa337483e9a9caef
Andreas Jacobsen <andreas1300@gmail.com>
2019-05-22 21:54:17 +0200

    start of tyler mcginnis course

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//utenfor 'react' så her er det ren JS
+// denne isLoading er ikke ekte, men virker
+function isLoading() {
+  return false;
+}
+
+function App() {
+const name = 'Andreas'
+// får hele function App til å returnere null om er sann
+// som gjør at ingenting i funksjonen App vises
+if(isLoading() === true) {
+  return null
+}
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+          
+          <h1>Hei verden, jeg er skrevet av {name}</h1>
+          <p>Idag er det {new Date().toLocaleDateString()}</p>
+          <p> 2 ganger to er {2*2}</p>
+        
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
